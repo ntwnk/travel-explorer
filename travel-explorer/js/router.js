7fb51b27da44bde367f8cf1c0c1b57b6d932f9ff
nikki-dev <80328391+ntwnk@users.noreply.github.com>
2026-06-21 12:41:49 +1000

    Delete travel-explorer directory

--- travel-explorer/js/router.js
+++
@@ -1,9 +0,0 @@
-const routes = [
-  { path: "/", component: JobOverview },
-  { path: "/job/:id", component: JobDetail }
-];
-
-const router = VueRouter.createRouter({
-  history: VueRouter.createWebHashHistory(),
-  routes
-});
